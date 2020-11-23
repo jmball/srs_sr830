3e601aa8af7602953d766da0b979a698f798674a
Greyson Christoforo <grey@christoforo.net>
2020-11-23 16:44:01 +0000

    PEP517 compliant packaging

--- setup.py
+++ setup.py
@@ -1,22 +1,4 @@
-import setuptools
+from setuptools import setup
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
 
-setuptools.setup(
-    name="srs_sr830",
-    use_scm_version=True,
-    setup_requires=["setuptools_scm"],
-    author="James Ball",
-    author_email="",
-    description="Stanford Research Systems SR830 control library",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/jmball/srs_sr830",
-    py_modules=["sr830", "virtual_sr830"],
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT",
-        "Operating System :: OS Independent",
-    ],
-)
+setup()
